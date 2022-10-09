f769ced79d58e9eccf6a0bdc18f016df07267943
Qobil_Urinboeff <qobilurinboev98@gmail.com>
2022-10-09 20:57:10 +0500

    forms and slides

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 import modal from './modules/modal';
-
+import slider from './modules/slider';
+import form from './modules/forms';
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
-
     modal();
+    slider('.feedback-slider-item', '', '.main-prev-btn', '.main-next-btn');
+    slider('.main-slider-item', 'vertical');
+    form();
 });
